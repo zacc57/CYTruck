4d46949dc25e01676d25843cdc9a9b7eb780bbac
zacc57 <154211273+zacc57@users.noreply.github.com>
2024-01-18 00:16:44 +0100

    Update annexe.c

--- progc/annexe.c
+++ progc/annexe.c
@@ -4,45 +4,71 @@
 
 
 
-typedef struct {
-    int numero_etape;
-    char ville_depart[50];
-    char ville_arrivee[50];
-    int distance;
-} Etape;
+typedef struct AVL{
+    AVL* FD;
+    AVL* FG;
+    char** ville;
+    int NbrTraverse;
+    int NbrDepart;
+} AVL;
 
-
-typedef struct Node {
-    Etape etape;
-    struct Node* left;
-    struct Node* right;
-    int height;
-} Node;
-
-
-
-Node* createNode(Etape etape) {
-    Node* newNode = (Node*)malloc(sizeof(Node));
-    newNode->etape = etape;
-    newNode->left = NULL;
-    newNode->right = NULL;
-    newNode->height = 1;  // Nouveau nœud a une hauteur de 1
-    return newNode;
-}
-
-
-int getBalance(Node* node) {
-    if (node == NULL)
-        return 0;
-    return height(node->left) - height(node->right);
-}
-
-
-
-void freeTree(Node* root) {
-    if (root != NULL) {
-        freeTree(root->left);
-        freeTree(root->right);
-        free(root);
+AVL* NewAVL(Char** a, ){
+    AVL* pNew=Malloc(sizeof(AVL)); //possible remplacement par stlern
+    if (pNew=NULL){
+      exit(1);
     }
+    pNew->ville=a*;
+    pNew->NbrTRaverse=0;
+    pNew->NbrDepart=0;
+    pNew->FD=NULL;
+    pNEw->FG=NULL;
+    return pNew;
 }
+
+int Main{
+
+    Fonction arbre vide ?
+    While (a*!="\0"){
+      char a* = scan la ville;
+      short b = scanf(etape=1)// b=1 si c'est un dÃ©part 0 sinon
+      TrierAVL1(a*, *pHead, b)
+      equilibreAVL
+      ligne suivante
+    }
+
+
+  AVL* TrierAVL1(char* a, AVL* pAVL, short b){
+        if (a*<pAVL->ville){
+            if (pAVL->FG=NULL){
+                AVL* pNew=AddAVL1(*a, b);
+            }
+            else{
+                TrierAVL1(*a, *FG, b);
+            }
+        }
+        else if (a*>pAVL->ville){
+            if (pAVL->FD=NULL){
+                AVL* pNew=AddAVL1(*a, b);
+            }
+            else{
+                TrierAVL1(*a, *FD, b);
+            }
+        }
+        else {
+            pAVL->NbrTraverse++;
+            if (b=1){
+                pNew->NbrDepart++;
+            }
+        }
+        return 0;
+  }
+
+  AVL* AddAVL1(char* a, short b){
+
+      AVL* pNew=NewAVL(*a);
+      pNew->NbrTraverse++;
+      if (b=1){
+          pNew->NbrDepart++;
+      }
+      return pNew;
+  }
