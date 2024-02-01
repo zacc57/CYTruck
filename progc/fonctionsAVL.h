e02021b292b64fa7297e1fab064ee954909b6fc0
zacc57 <154211273+zacc57@users.noreply.github.com>
2024-02-01 19:58:02 +0100

    Update fonctionsAVL.h

--- progc/fonctionsAVL.h
+++ progc/fonctionsAVL.h
@@ -1,1 +1,18 @@
+#ifndef FONCTIONSAVLC_H
+#define FOONCTIONSAVLC_H
 
+AVL* rotationDroite(AVL* noeud)
+
+AVL* rotationGauche(AVL* noeud)
+
+equilibrage(AVL* noeud)
+
+AVL* insertionAVL(AVL* noeud, char* ville, int nombre_trajet, int nombre_depart)
+
+void parcoursInfixe(AVL* noeud)
+
+int max(int a, int b)
+
+int hauteur(struct AVL* noeud)
+
+#endif
